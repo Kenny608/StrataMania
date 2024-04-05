00e4f2f236c2e907855e7f1030684ce9565a115b
Kenny608 <63941432+Kenny608@users.noreply.github.com>
2024-04-04 18:20:46 -0700

    Adding base files

---
+++ input_Warrior/input_Warrior/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  input_Warrior
+//
+//  Created by Kenny Luchau on 4/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello HELLDIVERS!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ input_Warrior/input_Warrior/input_WarriorApp.swift
@@ -0,0 +1,17 @@
+//
+//  input_WarriorApp.swift
+//  input_Warrior
+//
+//  Created by Kenny Luchau on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct input_WarriorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
